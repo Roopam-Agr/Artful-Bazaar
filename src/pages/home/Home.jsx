1629334a174ffcdbf059303596067ce70e09bc8b
Roopam-Agr <agrawalroopam438@gmail.com>
2024-04-15 13:21:42 +0530

    Product card created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,15 @@
 import React, { useContext } from "react"
 import Layout from "../../components/layout/Layout"
-import MyContext from "../../context/data/MyContext"
 import HeroSection from "../../components/heroSection/HeroSection"
 import Filter from "../../components/filter/Filter"
+import ProductCard from "../../components/productCard/ProductCard"
 
 function Home() {
   return (
     <Layout>
       <HeroSection />
       <Filter />
+      <ProductCard />
     </Layout>
   )
 }
