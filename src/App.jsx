bc2eb106f48085ff71666bf6d8b33c2e469af36a
Roopam-Agr <agrawalroopam438@gmail.com>
2024-04-15 14:04:14 +0530

    Product info page and cart page created

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import MyState from "./context/data/MyState"
 import Login from "./pages/registration/Login"
 import Signup from "./pages/registration/Signup"
 
+import ProductInfo from "./pages/productInfo/ProductInfo"
+
 function App() {
   return (
     <MyState>
@@ -23,6 +25,8 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/productinfo/:id" element={<ProductInfo />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="/*" element={<NoPage />} />
         </Routes>
       </Router>
