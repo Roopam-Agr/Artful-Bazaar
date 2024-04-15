19db3ce8f30eb374ca6ff8921e3389f178bc7120
Roopam-Agr <agrawalroopam438@gmail.com>
2024-04-15 13:16:00 +0530

    Filter Component completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,13 @@ import React, { useContext } from "react"
 import Layout from "../../components/layout/Layout"
 import MyContext from "../../context/data/MyContext"
 import HeroSection from "../../components/heroSection/HeroSection"
+import Filter from "../../components/filter/Filter"
+
 function Home() {
   return (
     <Layout>
       <HeroSection />
+      <Filter />
     </Layout>
   )
 }
