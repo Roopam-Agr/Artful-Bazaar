ee48358b0611c39e13431dfe9c52f0e7fb950c07
Roopam-Agr <agrawalroopam438@gmail.com>
2024-04-15 11:57:37 +0530

    created navbar for desktop

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,17 +2,7 @@ import React, { useContext } from "react"
 import Layout from "../../components/layout/Layout"
 import MyContext from "../../context/data/MyContext"
 function Home() {
-  const context = useContext(MyContext)
-  console.log(context)
-  //   const { name, rollno } = context
-  const { state, color } = context
-  return (
-    <Layout>
-      <h1>Name : {state.name}</h1>
-      <h1>Roll No. : {state.rollno}</h1>
-      <h1>My color : {color}</h1>
-    </Layout>
-  )
+  return <Layout>Home</Layout>
 }
 
 export default Home
