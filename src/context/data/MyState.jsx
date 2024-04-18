11bc30b781bce03b08cfb4d3a50b6a3745279afd
Roopam-Agr <agrawalroopam438@gmail.com>
2024-04-18 12:14:30 +0530

    created addproduct and getproduct function

--- src/context/data/MyState.jsx
+++ src/context/data/MyState.jsx
@@ -1,5 +1,14 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import MyContext from "./MyContext"
+import {
+  Timestamp,
+  collection,
+  addDoc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore"
+import { fireDB } from "../../firebase/FirebaseConfig"
 
 function MyState(props) {
   const [mode, setMode] = useState("light")
@@ -16,8 +25,86 @@ function MyState(props) {
 
   const [loading, setLoading] = useState(false)
 
+  const [products, setProducts] = useState({
+    title: null,
+    price: null,
+    imageUrl: null,
+    category: null,
+    description: null,
+    time: Timestamp.now(),
+    date: new Date().toLocaleString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    }),
+  })
+
+  const addProduct = async () => {
+    if (
+      products.title == null ||
+      products.price == null ||
+      products.imageUrl == null ||
+      products.category == null ||
+      products.description == null
+    ) {
+      return toast.error("Please fill all fields")
+    }
+
+    setLoading(true)
+    try {
+      const productRef = collection(fireDB, "products")
+      await addDoc(productRef, products)
+      toast.success("Product Add successfully")
+      getProductData()
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  const [product, setProduct] = useState([])
+
+  const getProductData = async () => {
+    setLoading(true)
+    try {
+      const q = query(
+        collection(fireDB, "products"),
+        orderBy("time")
+        // limit(5)
+      )
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let productArray = []
+        QuerySnapshot.forEach((doc) => {
+          productArray.push({ ...doc.data(), id: doc.id })
+        })
+        setProduct(productArray)
+        setLoading(false)
+      })
+      return () => data
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getProductData()
+  }, [])
+
   return (
-    <MyContext.Provider value={{ mode, toggleMode, loading, setLoading }}>
+    <MyContext.Provider
+      value={{
+        mode,
+        toggleMode,
+        loading,
+        setLoading,
+        products,
+        setProducts,
+        addProduct,
+        product,
+      }}
+    >
       {props.children}
     </MyContext.Provider>
   )
